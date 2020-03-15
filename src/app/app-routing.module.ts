f581a8127bb8007207b6a878c6115c5f86d2a4e9
glowbowl <lyashko2@gmail.com>
2020-03-15 16:16:15 +0200

    innit comm

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { InfoComponent } from './info/info.component';
+import { CreateComponent } from './create/create.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainComponent},
+  { path: 'info', component: InfoComponent },
+  { path: 'create', component:CreateComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { InfoComponent } from './info/info.component';
+import { CreateComponent } from './create/create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    InfoComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule,
